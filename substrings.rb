e6a22a66fbca8d6e10cddc1a30dc28c2c8b704fd
stefanIvasca <stefanivasca@gmail.com>
2022-11-26 15:38:19 +0200

    find word from dictionary in a single word string

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,19 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 string = 'below'
 
 def substrings(string, dictionary)
-
+    result = 0
+    hash = {}
+    dictionary.each do |word|
+        if string.include? word
+            if hash.include? word
+                hash[word] += 1
+            else 
+                hash[word] = 1
+            end
+        end
+    end
+    puts hash
 end
+
+substrings(string, dictionary)
