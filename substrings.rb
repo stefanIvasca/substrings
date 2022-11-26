8bea62282e0aa60a63dec6e3e4b68c61eebb84cf
stefanIvasca <stefanivasca@gmail.com>
2022-11-26 17:49:56 +0200

    make it work for string with multiple words

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,17 @@
 dictionary = ["below","down","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-string = 'below'
+string = 'below low go go go low horngo horn'
 
 def substrings(string, dictionary)
-    result = 0
+    array_from_string = string.split(" ")
     hash = {}
     dictionary.each do |word|
-        if string.include? word
-            if hash.include? word
-                hash[word] += 1
-            else 
-                hash[word] = 1
+        array_from_string.each do |string_word|
+            if string_word.include? word
+                if hash.has_key? (word)
+                    hash[word] += 1
+                else 
+                    hash[word] = 1
+                end
             end
         end
     end
